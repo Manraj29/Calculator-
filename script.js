00041cc0b29d5eb2542a88ab409cb42d075e5b5d
Manraj Singh Virdi <61323049+Manraj29@users.noreply.github.com>
2021-11-16 19:29:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
 // Use insert() function to insert the number in textview.  
-function insert(num) {  
-  document.getElementById('current-operand').innerHTML = document.getElementById('current-operand').innerHTML+num;
-  document.getElementById('previous-operand').innerHTML = "";
-
-}  
+function insert(num, curop) {
+	document.getElementById(curop).innerHTML = document.getElementById(curop).innerHTML + num;
+	document.getElementById('previous-operand').innerHTML = "";
+}
 
 // Use equal() function to return the result based on passed values.  
-function equal()  {  
-  var exp = document.getElementById('current-operand').innerHTML;  
-  if(exp != " ")  {  
-    document.getElementById('previous-operand').innerHTML = document.getElementById('current-operand').innerHTML; 
-    document.getElementById('current-operand').innerHTML = eval(exp);
-    addToHistory(eval(exp));
-  }  
-}  
+function equal(curop, preop, query, hist) {
+	var exp = document.getElementById(curop).innerHTML;
+	if (exp != " ") {
+		document.getElementById(preop).innerHTML = document.getElementById(curop).innerHTML;
+		document.getElementById(curop).innerHTML = eval(exp);
+		addToHistory(eval(exp), preop, query, hist);
+	}
+}
 
 // For backspace
-function backspace() {  
-  document.getElementById('previous-operand').innerHTML = "";
-  var exp = document.getElementById('current-operand').innerHTML;  
-  document.getElementById('current-operand').innerHTML = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */  
-}  
+function backspace(curop, preop) {
+	document.getElementById(preop).innerHTML = "";
+	var exp = document.getElementById(curop).innerHTML;
+	document.getElementById(curop).innerHTML = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */
+}
 
 //Clear all
-function clearall() {
-  document.getElementById('current-operand').innerHTML = ''; 
-  document.getElementById('previous-operand').innerHTML = ''; 
+function clearall(curop, preop) {
+	document.getElementById(curop).innerHTML = '';
+	document.getElementById(preop).innerHTML = '';
 }
+
 //change Theme
 function changetheme() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-  var chngoutput = document.getElementById('chnge-output');
-  chngoutput.classList.toggle("dark-output");
-  if(document.getElementById('lighttheme').style.display == "block"){
-    document.getElementById('lighttheme').style.display = "none";
-    document.getElementById('darktheme').style.display = "block";
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+	var chngoutput = document.getElementById('chnge-output');
+	chngoutput.classList.toggle("dark-output");
+	if (document.getElementById('lighttheme').style.display == "block") {
+		document.getElementById('lighttheme').style.display = "none";
+		document.getElementById('darktheme').style.display = "block";
 
-  } else{
-    document.getElementById('lighttheme').style.display = "block";
-    document.getElementById('darktheme').style.display = "none";
+	} else {
+		document.getElementById('lighttheme').style.display = "block";
+		document.getElementById('darktheme').style.display = "none";
 
-  }
+	}
 }
 
 //Opening Navbar
 function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "0px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	document.getElementById("mySidenav").style.width = "300px";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 //Closing Navbar
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.body.style.backgroundColor = "white";
+	document.getElementById("mySidenav").style.width = "0";
+	document.body.style.backgroundColor = "white";
 }
 
 
@@ -64,47 +62,222 @@ let history = [''];
 let data = [''];
 var arr = [];
 
-function addToHistory(value) {
-  var total = '';
-  arr = [];
+function addToHistory(value, preop, query, hist) {
+	var total = '';
+	arr = [];
 
-  history.push(value); 
-  data.push(document.getElementById('previous-operand').innerHTML);
-  for(var i = 0; i <history.length; i++){
-    arr.push(data[i] + " = " + history[i] + " ");
-    // window.alert(arr[i]);
-  }
-  document.getElementById('history').innerHTML = " ";
-  document.getElementById('query').innerHTML = " ";
+	history.push(value);
+	data.push(document.getElementById(preop).innerHTML);
+	for (var i = 0; i < history.length; i++) {
+		arr.push(data[i] + " = " + history[i] + " ");
+		// window.alert(arr[i]);
+	}
+	document.getElementById(hist).innerHTML = " ";
+	document.getElementById(query).innerHTML = " ";
 
-  for (var j=arr.length-1; j > 0; j--){
-    // document.getElementById('query').innerHTML += "<br>" + history[j] + "=";
-    // document.getElementById('history').innerHTML += "<br>" + data[j] + "<hr>";
-    document.getElementById('history').innerHTML += "" + arr[j] + "<hr>";
-
-  } 
+	for (var j = arr.length - 1; j > 0; j--) {
+		document.getElementById(hist).innerHTML += "" + arr[j] + "<hr>";
+	}
 }
+
 
 //Clearing History
-function clearhistory() {
+function clearhistory(query, hist) {
 
-  while(history.length > 1) {
-    arr.pop();
-    history.pop();
-    data.pop();
-  }
-  // window.alert(history.length);
-  document.getElementById('history').innerHTML = " ";
-  document.getElementById('query').innerHTML = " ";
+	while (history.length > 1) {
+		arr.pop();
+		history.pop();
+		data.pop();
+	}
+	document.getElementById(hist).innerHTML = " ";
+	document.getElementById(query).innerHTML = " ";
 
 }
 
+
+//Show History mobile view with cookies
 function showhistory() {
-  if(document.getElementById('operation-history').style.display == "block"){
-    document.getElementById('operation-history').style.display = "none";
-  } else{
-    document.getElementById('operation-history').style.display = "block";
-  }
-  var element = document.getElementById('operation-history');
-  element.classList.toggle("display-operation-history");
+	if (localStorage.getItem('currentCalculator') == 'standard') {
+		if (document.getElementById('operation-history').style.display == "block") {
+			document.getElementById('operation-history').style.display = "none";
+		} else {
+			document.getElementById('operation-history').style.display = "block";
+		}
+		var element = document.getElementById('operation-history');
+		element.classList.toggle("display-operation-history");
+	} 
+	else if (localStorage.getItem('currentCalculator') == 'programmer') {
+		if (document.getElementById('pro-operation-history').style.display == "block") {
+			document.getElementById('pro-operation-history').style.display = "none";
+		} else {
+			document.getElementById('pro-operation-history').style.display = "block";
+		}
+		var element = document.getElementById('pro-operation-history');
+		element.classList.toggle("pro-display-operation-history");
+	}
+	else if (localStorage.getItem('currentCalculator') == 'scientific') {
+		if (document.getElementById('sci-operation-history').style.display == "block") {
+			document.getElementById('sci-operation-history').style.display = "none";
+		} else {
+			document.getElementById('sci-operation-history').style.display = "block";
+		}	
+	}
+
 }
+
+
+//dates calc
+function dropdowndate(value) {
+	if (value == "difference") {
+		document.getElementById('id-date-cal').style.display = "none";
+		document.getElementById('id-dates-diff').style.display = "block";
+	}
+	else if (value == "calculations") {
+		document.getElementById('id-date-cal').style.display = "block";
+		document.getElementById('id-dates-diff').style.display = "none";
+	}
+}
+
+function dateDiff() {
+	var d = new Date(document.getElementById('date-from').value);
+    var d2 = new Date(document.getElementById('date-to').value);
+	// alert(d);
+    var result = document.getElementById('result');
+	document.getElementById("diff-dates").style.display = "block";
+	document.getElementById('diff-dates').innerHTML = "Difference of " + Math.ceil(Math.abs(d2 - d) / (1000 * 60 * 60 * 24)) + " days";
+}
+
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+function subdates() {
+	var d = new Date(document.getElementById('date-cal-from').value);
+	var no = document.getElementById('opdate').value;
+	// alert(no);
+	d.setDate(d.getDate() - no);
+	document.getElementById("cal-dates").style.display = "block";
+	document.getElementById("cal-dates").innerHTML = "Date: <br>" + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + "<br>Day: " + weekday[d.getDay()];
+}
+
+function adddates() {
+	var d = new Date(document.getElementById('date-cal-from').value);
+	var no = parseInt(document.getElementById('opdate').value);
+	// alert(no);
+	d.setDate(d.getDate() + no);
+	document.getElementById("cal-dates").style.display = "block";
+	document.getElementById("cal-dates").innerHTML = "Date: <br>" + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + "<br>Day: " + weekday[d.getDay()];
+}
+
+function cleardates(from){
+	document.getElementById(from).value = null;
+    document.getElementById('date-to').value = null;
+	document.getElementById("cal-dates").innerHTML = "";
+	document.getElementById("diff-dates").innerHTML = ""
+	document.getElementById("opdate").value = ""
+	var op = document.getElementsByName("opr");
+	for (var i = 0; i < op.length; i++) {
+		op[i].checked = false;
+	}
+}
+
+//Currency Converter
+const from_currencyEl = document.getElementById('from_currency');
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.getElementById('to_currency');
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+const exchange = document.getElementById('exchange');
+
+exchange.addEventListener('click', () => {
+	const temp = from_currencyEl.value;
+	from_currencyEl.value = to_currencyEl.value;
+	to_currencyEl.value = temp;
+	calculate();
+});
+ 
+function calculate() {
+	const from_currency = from_currencyEl.value;
+	const to_currency = to_currencyEl.value; 
+	document.getElementById('con-from-to').innerHTML = "Conversion from " + from_currency + " to " + to_currency;
+	fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`) //all the exchange rates from the from_currency currency
+	.then(res => res.json())
+	.then(res => {
+	const rate = res.rates[to_currency]; //get the rate of the to_currency currency
+	rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}` //display the rate
+	to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2); //have 2 digits after decimal and display the result
+	})
+}
+calculate();
+
+//Navbar Elements
+
+function showProgrammer() {
+	document.getElementById('standard').style.display = "none";
+	document.getElementById('Scientific').style.display = "none";
+	document.getElementById('Date-Difference').style.display = "none";
+	document.getElementById('Programmer').style.display = "block";
+	document.getElementById('currency-con').style.display = "none";
+	document.getElementById('show-history-btn').style.display = "block";
+
+	localStorage.setItem('currentCalculator', 'programmer');
+	document.getElementById('calc-type').innerHTML = "Programmer";
+	closeNav();
+}
+
+function showStandard() {
+	document.getElementById('standard').style.display = "block";
+	document.getElementById('Programmer').style.display = "none";
+	document.getElementById('Scientific').style.display = "none";
+	document.getElementById('Date-Difference').style.display = "none";
+	document.getElementById('currency-con').style.display = "none";
+	document.getElementById('show-history-btn').style.display = "block";
+
+	localStorage.setItem('currentCalculator', 'standard');
+	document.getElementById('calc-type').innerHTML = "Standard";
+	closeNav();
+}
+
+function showScientific() {
+	document.getElementById('Scientific').style.display = "block";
+	document.getElementById('standard').style.display = "none";
+	document.getElementById('Date-Difference').style.display = "none";
+	document.getElementById('Programmer').style.display = "none";
+	localStorage.setItem('currentCalculator', 'scientific');
+	document.getElementById('currency-con').style.display = "none";
+	document.getElementById('show-history-btn').style.display = "block";
+
+	document.getElementById('calc-type').innerHTML = "Scientific";
+	closeNav();
+}
+
+function showDateDiff() {
+	document.getElementById('Scientific').style.display = "none";
+	document.getElementById('Date-Difference').style.display = "block";
+	document.getElementById('standard').style.display = "none";
+	document.getElementById('Programmer').style.display = "none";
+	document.getElementById('currency-con').style.display = "none";
+	document.getElementById('show-history-btn').style.visibility = "none";
+
+	localStorage.setItem('currentCalculator', 'dateDiff');
+	document.getElementById('calc-type').innerHTML = "Date Calculation";
+	closeNav();
+}
+
+function showCurrencyCon() {
+	document.getElementById('Scientific').style.display = "none";
+	document.getElementById('currency-con').style.display = "block";
+	document.getElementById('Date-Difference').style.display = "none";
+	document.getElementById('standard').style.display = "none";
+	document.getElementById('Programmer').style.display = "none";
+	document.getElementById('show-history-btn').style.visibility = "none";
+	localStorage.setItem('currentCalculator', 'dateDiff');
+	document.getElementById('calc-type').innerHTML = "Currency Con";
+	closeNav();
+}
+
